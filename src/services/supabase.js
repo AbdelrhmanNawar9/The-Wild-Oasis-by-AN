139647a3d813bf88fe272daa92f4ec22d8632525
unknown <abdonawar97@gmail.com>
2024-12-18 21:36:01 +0200

    Initial commit

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://ytwgbmnfqydwmdexqzvz.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0d2dibW5mcXlkd21kZXhxenZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4MDQ4MDYsImV4cCI6MjA0NjM4MDgwNn0.o7qv_g1YEIMzov-xdQ5CHMH7FLWg67Kp6jA_TbtVx0U";
 
+export const supabaseUrl = "https://ytwgbmnfqydwmdexqzvz.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
