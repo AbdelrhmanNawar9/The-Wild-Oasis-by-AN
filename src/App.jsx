014d4c4fb52c92403d92a170ceb3d52e73cfde85
unknown <abdonawar97@gmail.com>
2024-12-18 21:37:53 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard.jsx";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
 import Users from "./pages/Users";
